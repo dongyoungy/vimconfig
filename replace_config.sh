3f584069b2ff5bb845afec829550287d80509acf
Dong Young Yoon <dongyoungy@gmail.com>
2017-09-11 09:23:36 -0400

    added ycm_extra_conf.py

--- replace_config.sh
+++ replace_config.sh
@@ -4,12 +4,14 @@
 rm ~/.vimrc.local
 rm ~/.vimrc.bundles.local
 rm ~/.vimrc.before.local
+rm ~/.ycm_extra_conf.py
 
 current_dir=$PWD
 
 cd ~ && ln -s $current_dir/.vimrc.local .vimrc.local
 cd ~ && ln -s $current_dir/.vimrc.bundles.local .vimrc.bundles.local
 cd ~ && ln -s $current_dir/.vimrc.before.local .vimrc.before.local
+cd ~ && ln -s $current_dir/.ycm_extra_conf.py .ycm_extra_conf.py
 
 mkdir -p ~/.vim/colors
 cp $current_dir/base16-railscasts.vim ~/.vim/colors
